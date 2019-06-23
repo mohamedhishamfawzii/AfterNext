9edde032d2385e0a41e9c9f64da21f2fd568a24c
Mahmoud Elkassry <Mahmoud3341@gmail.com>
2019-06-23 13:24:36 +0200

    - added player home view

---
+++ After Next Player/After Next Player/ArenasCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArenasCollectionViewCell.swift
+//  After Next Player
+//
+//  Created by mahmoud mohamed on 6/23/19.
+//  Copyright © 2019 hisham. All rights reserved.
+//
+
+import UIKit
+
+class ArenasCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
---
+++ After Next Player/After Next Player/PlayerHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerHomeView.swift
+//  After Next Player
+//
+//  Created by mahmoud mohamed on 6/23/19.
+//  Copyright © 2019 hisham. All rights reserved.
+//
+
+import UIKit
+
+class PlayerHomeView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+    }
+
+}

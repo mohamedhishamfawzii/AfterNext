ce643396ce9a82dc0421ccb6be89747ba0c746f7
Mohamed Hisham <m.hisham@live.com>
2019-06-23 11:21:31 +0200

    initial project

---
+++ After Next Player/After Next Player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  After Next Player
+//
+//  Created by mohamed hisham on 6/23/19.
+//  Copyright © 2019 hisham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
